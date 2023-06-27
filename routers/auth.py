81dee83c30ab56e67257a67c3968f764f9c88253
theMcSam <ardisam9876@gmail.com>
2023-06-27 11:51:06 +0000

    created signup and login routes

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,56 @@
-from fastapi import APIRouter
-from schemas.auth import UserLogin
+from fastapi import APIRouter, HTTPException
+from schemas.auth import UserLogin, UserSignUp
+from blog.database import db_session
+from blog.models import User
+
+from uuid import uuid4
+import os
+import bcrypt
+import jwt
+
 
 auth = APIRouter()
+JWT_SECRET_KEY = os.environ.get("JWT_SECREY_KEY")
 
 @auth.post("/api/auth/login")
 def login(user: UserLogin):
-    ...
+    user = db_session.query(User).filter(User.username == user.username).first()
+
+    if user and bcrypt.checkpw(user.password.encode(), str(user.password).encode()):
+        token=""
+        if user.is_admin:
+            jwt_payload = {"username": user.username, "user_id": user.user_id}
+            token = jwt.encode(jwt_payload, JWT_SECRET_KEY, algorithm="HS256")
+
+        return {
+            "msg":"Logged in successfully.",
+            "user_id": user.user_id,
+            "token": token
+        }
+       
+
+    raise HTTPException(status_code=404, detail="Username or password is incorrect.")
+
+@auth.post("/api/auth/signup")
+def sign_up(user: UserSignUp):
+    user_id = str(uuid4())
+
+    username_exists = db_session.query(User).filter(User.username == user.username).first()
+
+    if username_exists:
+        raise HTTPException(status_code=400, detail="Duplicate usernames not allowed.")
+
+    email_exists = db_session.query(User).filter(User.email == user.email).first()
+
+    if  email_exists:
+        raise HTTPException(status_code=400, detail="Email already exits.")
+    
+    hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt(12)).decode()
+
+    user = User(user_id, user.username, hashed_password, user.email)
+    db_session.add(user)
+    db_session.commit()
+
+    return {
+        "msg" : "User registered successfully."
+    }
--- schemas/auth.py
+++ schemas/auth.py
@@ -3,3 +3,6 @@ from pydantic import BaseModel
 class UserLogin(BaseModel):
     username: str
     password: str
+
+class UserSignUp(UserLogin):
+    email: str
