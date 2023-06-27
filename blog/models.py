75b566158104fc371cb0ddc2f83120db2cdf1106
theMcSam <ardisam9876@gmail.com>
2023-06-27 11:08:16 +0000

    created the post model

--- blog/models.py
+++ blog/models.py
@@ -9,12 +9,29 @@ class User(Base):
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String, nullable=True)
-    is_admin = Column(Boolean, nullable=False, default=False)
     date_created = Column(DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, user_id, username, password, email, is_admin):
+    def __init__(self, user_id, username, password, email):
         self.user_id = user_id
         self.username = username
         self.password = password
         self.email = email
-        self.is_admin = is_admin
+
+class Post(Base):
+    __tablename__ = "posts"
+    post_id = Column(String, nullable=False, primary_key=True)
+    title = Column(String, nullable=False)
+    views = Column(Integer, nullable=False, default=0)
+    post_author = Column(String, nullable=False, ForeignKey=True)
+    date_created = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, post_id, title, views, post_author):
+        self.post_id = post_id
+        self.title = title
+        self.views = views
+        self.post_author = post_author
+# blog model
+# author
+# comments
+# views
+# likes
