28646cdb5e2c64f60f5ef1715bcb8b2cc60a2639
theMcSam <ardisam9876@gmail.com>
2023-06-27 10:44:22 +0000

    created .env file to store sensitive credentials

---
+++ blog/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+import os
+
+load_dotenv()
+
+Base = declarative_base()
+
+engine = create_engine(
+    os.environ.get('RENDER_POSTRGRESS_DB_URL'), echo=True
+)
+
+Session = sessionmaker(bind=engine)
+db_session =  Session()
